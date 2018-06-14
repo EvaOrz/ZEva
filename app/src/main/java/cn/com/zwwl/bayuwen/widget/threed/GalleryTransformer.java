9609761ec4b8b1ced331ab880e009cc38391e271
鲁思圆 <lusiyuan@zhongwenweilai.com>
2018-06-14 18:04:36 +0800

    ss

--- app/src/main/java/cn/com/zwwl/bayuwen/widget/threed/GalleryTransformer.java
+++ app/src/main/java/cn/com/zwwl/bayuwen/widget/threed/GalleryTransformer.java
@@ -1,5 +1,6 @@
 package cn.com.zwwl.bayuwen.widget.threed;
 
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
@@ -10,12 +11,14 @@ public class GalleryTransformer implements ViewPager.PageTransformer {
     public void transformPage(View view, float position) {
         float scale = 0.5f;
         float scaleValue = 1 - Math.abs(position) * scale;
-        Log.e("sssssssss",Math.abs(position) +"");
+        Log.e("sssssssss", Math.abs(position) + "");
         view.setScaleX(scaleValue);
         view.setScaleY(scaleValue);
         view.setAlpha(scaleValue);
         view.setPivotX(view.getWidth() * (1 - position - (position > 0 ? 1 : -1) * 0.75f) * scale);
         view.setPivotY(view.getHeight() / 2);
+        if (Build.VERSION.SDK_INT >= 21)
+            view.setElevation(position > -0.25 && position < 0.25 ? 1 : 0);
 
     }
 }
