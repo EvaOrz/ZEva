0f80d7a548bb5479a7774d2fb17c5095d63b232f
zhumangmang <971338492@qq.com>
2018-07-02 18:29:18 +0800

    update

--- app/src/main/java/cn/com/zwwl/bayuwen/base/MenuCode.java
+++ app/src/main/java/cn/com/zwwl/bayuwen/base/MenuCode.java
@@ -4,4 +4,5 @@ public class MenuCode {
     public static final int SUBMIT=0;
     public static final int HIDE_CLASS = 1;
     public static final int SIGN =2 ;
+    public static final int HIDE_CANCEL = 3;
 }
